0e2fdda19e4dd910854f81bef0766bcdc21e96f1
ZekeJin97 <156149436+ZekeJin97@users.noreply.github.com>
2024-07-22 19:04:55 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 import boto3
 import time
 
-s3_client = boto3.client('s3')
-source_bucket = 's3backupsystemstack-sourcebucketddd2130a-pxthiplfvjtk'
+s3 = boto3.client('s3')
+source_bucket = 'copierstack-sourcebucketddd2130a-vc3snocinfuo'  # replace with your actual source bucket name
 
 def create_object(key, size_kb):
-    content = '0' * (1024 * int(size_kb))
-    s3_client.put_object(Bucket=source_bucket, Key=key, Body=content)
-    print(f"Uploaded {key} ({size_kb}KB)")
 
-# Upload objects
+    size_bytes = int(size_kb * 1024)
+    content = '0' * size_bytes
+    s3.put_object(Bucket=source_bucket, Key=key, Body=content)
+
+
 create_object('project.txt', 1)
-time.sleep(1)
+time.sleep(60)
 create_object('temp.txt', 1)
-time.sleep(1)
+time.sleep(60)
 create_object('project_new.txt', 1)
-time.sleep(1)
+time.sleep(60)
 create_object('temporary_data.txt', 2.5)
-time.sleep(1)
+time.sleep(60)
 create_object('project_new_new.txt', 1)
-time.sleep(1)
+time.sleep(60)
 create_object('real_temporary_data.txt', 2)
-
-print("All objects uploaded.")
+('real_temporary_data.txt', 2)

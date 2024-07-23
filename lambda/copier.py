e82af3a0399ac220f24ffe4553c5ab7782d22d52
ZekeJin97 <156149436+ZekeJin97@users.noreply.github.com>
2024-07-22 19:08:26 -0700

    Update copier.py

--- lambda/copier.py
+++ lambda/copier.py
@@ -1,14 +1,32 @@
-import boto3
 import os
+import boto3
 
 s3 = boto3.client('s3')
+cloudwatch = boto3.client('cloudwatch')
+destination_bucket = os.environ['DESTINATION_BUCKET']
+
 
 def handler(event, context):
-    source_bucket = event['Records'][0]['s3']['bucket']['name']
-    object_key = event['Records'][0]['s3']['object']['key']
-    destination_bucket = os.environ['DESTINATION_BUCKET']
+    print(f"Copier Lambda triggered with event: {event}")
 
-    copy_source = {'Bucket': source_bucket, 'Key': object_key}
-    s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=object_key)
+    for record in event['Records']:
+        source_bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        size = record['s3']['object']['size']
 
-    print(f"Copied {object_key} from {source_bucket} to {destination_bucket}")
+        copy_source = {'Bucket': source_bucket, 'Key': key}
+        s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=key)
+        print(f"Copied {key} from {source_bucket} to {destination_bucket}")
+
+        if 'temp' in key:
+            print(f"Temporary object {key} of size {size} bytes")
+            cloudwatch.put_metric_data(
+                Namespace='S3BackupSystem',
+                MetricData=[{
+                    'MetricName': 'TemporaryObjectSize',
+                    'Dimensions': [{'Name': 'BucketName', 'Value': destination_bucket}],
+                    'Unit': 'Bytes',
+                    'Value': size
+                }]
+            )
+            print(f"Published metric data for {key}")

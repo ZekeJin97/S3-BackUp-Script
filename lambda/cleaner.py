eed80a6824812732f55325bf23f95b18523e328c
ZekeJin97 <156149436+ZekeJin97@users.noreply.github.com>
2024-07-22 19:08:10 -0700

    Update cleaner.py

--- lambda/cleaner.py
+++ lambda/cleaner.py
@@ -1,20 +1,35 @@
-import boto3
 import os
+import boto3
 
 s3 = boto3.client('s3')
+destination_bucket = os.environ['DESTINATION_BUCKET']
+
 
 def handler(event, context):
-    destination_bucket = os.environ['DESTINATION_BUCKET']
-    temp_objects = []
+    print(f"Cleaner Lambda triggered with event: {event}")
 
-    response = s3.list_objects_v2(Bucket=destination_bucket)
+    # List objects in the destination bucket with 'temp' in the key
+    response = s3.list_objects_v2(
+        Bucket=destination_bucket,
+        Prefix='' 
+    )
 
-    for obj in response.get('Contents', []):
-        if 'temp' in obj['Key']:
-            temp_objects.append((obj['Key'], obj['LastModified']))
+    print(f"List objects response: {response}")
 
-    if temp_objects:
-        temp_objects.sort(key=lambda x: x[1])  # Sort by modification date
-        oldest_temp = temp_objects[0][0]
-        s3.delete_object(Bucket=destination_bucket, Key=oldest_temp)
-        print(f"Deleted {oldest_temp} from {destination_bucket}")
+    # Filter to get only temporary files
+    temp_files = [obj for obj in response.get('Contents', []) if 'temp' in obj['Key']]
+
+    print(f"Temporary files: {temp_files}")
+
+    if temp_files:
+        # Find the oldest temporary object
+        oldest = min(temp_files, key=lambda x: x['LastModified'])
+
+        # Delete the oldest temporary object
+        s3.delete_object(
+            Bucket=destination_bucket,
+            Key=oldest['Key']
+        )
+        print(f"Deleted {oldest['Key']}")
+    else:
+        print("No temporary objects found")

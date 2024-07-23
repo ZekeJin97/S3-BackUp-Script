36679fa495b6ce403f6af08c0a62ec61a3075c73
ZekeJin97 <156149436+ZekeJin97@users.noreply.github.com>
2024-07-22 19:05:52 -0700

    Update cleaner_stack.py

--- lib/cleaner_stack.py
+++ lib/cleaner_stack.py
@@ -1,25 +1,26 @@
 from aws_cdk import (
     Stack,
+    aws_iam as iam,
     aws_lambda as lambda_,
     aws_logs as logs,
     aws_cloudwatch as cloudwatch,
     aws_cloudwatch_actions as cw_actions,
+    Duration  # Import Duration from aws_cdk
 )
 from constructs import Construct
 
 class CleanerStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, destination_bucket_name: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        print(f"Initializing {construct_id}")
+    def __init__(self, scope: Construct, id: str, destination_bucket_name: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
         # Metric for the size of temporary objects
         temp_metric = cloudwatch.Metric(
             namespace="S3BackupSystem",
             metric_name="TemporaryObjectSize",
             dimensions_map={"BucketName": destination_bucket_name},
-            statistic="Sum"
+            statistic="Sum",
+            period=Duration.minutes(1)  # Set period to 1 minute
         )
 
         # Alarm to trigger the cleaner lambda
@@ -28,7 +29,7 @@ class CleanerStack(Stack):
                                  threshold=3 * 1024,  # 3KB
                                  evaluation_periods=1,
                                  datapoints_to_alarm=1
-        )
+                                 )
 
         # Lambda function to clean temporary objects
         cleaner_lambda = lambda_.Function(self, "CleanerLambda",
@@ -38,7 +39,13 @@ class CleanerStack(Stack):
                                           environment={
                                               'DESTINATION_BUCKET': destination_bucket_name
                                           }
-        )
+                                          )
+
+        # Permissions for the lambda function
+        cleaner_lambda.add_to_role_policy(iam.PolicyStatement(
+            actions=["s3:ListBucket", "s3:DeleteObject"],
+            resources=[f"arn:aws:s3:::{destination_bucket_name}", f"arn:aws:s3:::{destination_bucket_name}/*"]
+        ))
 
         # Alarm action
         alarm.add_alarm_action(cw_actions.LambdaAction(cleaner_lambda))
@@ -47,7 +54,5 @@ class CleanerStack(Stack):
         logs.LogGroup(self, "CleanerLogGroup",
                       log_group_name=f"/aws/lambda/{cleaner_lambda.function_name}",
                       retention=logs.RetentionDays.ONE_WEEK
-        )
-
-        print(f"{construct_id} initialization complete")
+                      )
 

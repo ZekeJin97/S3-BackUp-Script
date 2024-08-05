876ba0e9e2a2f3aeedc2ee1e7b49db5ade928ee0
ZekeJin97 <156149436+ZekeJin97@users.noreply.github.com>
2024-08-05 13:25:27 -0700

    Delete s3-backup-system directory

--- s3-backup-system/cdk.out/asset.46ebe99053e3248fec0b13e35b8229b4ab7dc973e92579832878b159a112b701/log_dump.py
+++
@@ -1,9 +0,0 @@
-import json
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def handler(event, context):
-    for record in event['Records']:
-        logger.info(f"Received event: {json.dumps(record)}")
--- s3-backup-system/cdk.out/asset.6a68abe75d7d47d6b83eeaaf295e7d7139f989537730b03714e6214b26edf09e/logger.py
+++
@@ -1,9 +0,0 @@
-import json
-import logging
-
-# Set up logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def handler(event, context):
-    logger.info(f"Log Event: {json.dumps(event)}")
